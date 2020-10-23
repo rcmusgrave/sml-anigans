0603fee647755fd2bc1dba6960ec27ac8ca9f884
Ruth Musgrave <ruth@hormiguita.local>
2020-10-23 14:59:09 -0300

    Updates to plume code

--- hydroxide_plume.jl
+++ hydroxide_plume.jl
@@ -41,17 +41,17 @@ Pkg.instantiate()
 # than the boundary layer depth when the boundary layer penetrates half
 # the domain depth.
 
-Nh = 32
-Nz = 32
+Nh = 80
+Nz = 80
 
-grid = RegularCartesianGrid(size=(Nh, Nh, Nz), x=(0, 512), y=(0, 512), z=(-256, 0))
+grid = RegularCartesianGrid(size=(Nh, Nh, Nz), x=(0, 200), y=(0, 200), z=(-80, 0))
 
 # Buoyancy and boundary conditions
 
 Qᵇ = 1e-8
 Qᵘ = -1e-4
 
-surface_layer_depth = 32.0
+surface_layer_depth = 42.0
 thermocline_width = 24.0
 
 N²_surface_layer = 1e-6
@@ -113,7 +113,7 @@ T_sponge = Relaxation(rate = 1/hour,
                       target = LinearTarget{:z}(intercept = θ_deep - z_deep*dθdz_deep, gradient = dθdz_deep),
                       mask = gaussian_mask)
 
-hydroxide_source(x, y, z, t) = 1/hour * exp(-z^2 / 8 - (x^2 + y^2) / 200)
+OH⁻_source(x, y, z, t) = 1/hour * exp(-z^2 / 4 - ((x-100)^2 + (y-100)^2) / 10)
 
 # # LES Model
 
@@ -128,13 +128,13 @@ model = IncompressibleModel(architecture = CPU(),
                              timestepper = :RungeKutta3,
                                advection = WENO5(),
                                     grid = grid,
-                                 tracers = (:T, :hydroxide),
+                                 tracers = (:T, :OH⁻),
                                 buoyancy = buoyancy,
                                 coriolis = FPlane(f=1e-4),
                                  closure = AnisotropicMinimumDissipation(), #C=Cᴬᴹᴰ),
                      boundary_conditions = (T=θ_bcs, u=u_bcs),
                                  forcing = (u=u_sponge, v=v_sponge, w=w_sponge, T=T_sponge,
-                                            hydroxide=hydroxide_source)
+                                            OH⁻=OH⁻_source)
                                 )
 
 # # Set Initial condition
@@ -178,7 +178,7 @@ simulation = Simulation(model, Δt=wizard, stop_time=stop_hours * hour, iteratio
 
 # Prepare Output
 
-prefix = @sprintf("three_layer_constant_fluxes_Qu%.1e_Qb%.1e_Nh%d_Nz%d", abs(Qᵘ), Qᵇ, grid.Nx, grid.Nz)
+prefix = @sprintf("hydroxidePlume_Qu%.1e_Qb%.1e_Nh%d_Nz%d", abs(Qᵘ), Qᵇ, grid.Nx, grid.Nz)
 
 data_directory = joinpath(@__DIR__, "..", "data", prefix) # save data in /data/prefix
 
@@ -264,11 +264,13 @@ if make_animation
 
     xw, yw, zw = nodes(model.velocities.w)
     xu, yu, zu = nodes(model.velocities.u)
-    xc, yc, zc = nodes(model.tracers.c1)
+    xc, yc, zc = nodes(model.tracers.OH⁻)
+    xT, yT, zT = nodes(model.tracers.T)
 
     xw, yw, zw = xw[:], yw[:], zw[:]
     xu, yu, zu = xu[:], yu[:], zu[:]
     xc, yc, zc = xc[:], yc[:], zc[:]
+    xT, yT, zT = xT[:], yT[:], zT[:]
 
     #file = jldopen(simulation.output_writers[:slices].filepath)
     file = jldopen(joinpath(data_directory, prefix * "_slices.jld2"))
@@ -294,7 +296,7 @@ if make_animation
 
     @info "Making an animation from the saved data..."
 
-    anim = @animate for (i, iter) in enumerate(iterations)
+    anim = @animate for (i, iter) in enumerate(iterations[2:end])
 
         @info "Drawing frame $i from iteration $iter \n"
 
@@ -304,47 +306,68 @@ if make_animation
         w = file["timeseries/w/$iter"][:, 1, :]
         u = file["timeseries/u/$iter"][:, 1, :]
         v = file["timeseries/v/$iter"][:, 1, :]
-        c1 = file["timeseries/c1/$iter"][:, 1, :]
-        c3 = file["timeseries/c3/$iter"][:, 1, :]
-        d3 = file["timeseries/d3/$iter"][:, 1, :]
+        c1 = file["timeseries/OH⁻/$iter"][:, 1, :]
+        te = file["timeseries/T/$iter"][:, 1, :]
+        # c3 = file["timeseries/c3/$iter"][:, 1, :]
+        # d3 = file["timeseries/d3/$iter"][:, 1, :]
 
         U = statistics_file["timeseries/u/$iter"][1, 1, :]
         V = statistics_file["timeseries/v/$iter"][1, 1, :]
         E = statistics_file["timeseries/tke/$iter"][1, 1, :]
-        T = statistics_file["timeseries/T/$iter"][1, 1, :]
-        C1  = statistics_file["timeseries/c1/$iter"][1, 1, :]
-        C3  = statistics_file["timeseries/c3/$iter"][1, 1, :]
-        C12 = statistics_file["timeseries/c12/$iter"][1, 1, :]
-        C24 = statistics_file["timeseries/c24/$iter"][1, 1, :]
-        D1  = statistics_file["timeseries/d1/$iter"][1, 1, :]
-        D3  = statistics_file["timeseries/d3/$iter"][1, 1, :]
-        D12 = statistics_file["timeseries/d12/$iter"][1, 1, :]
-        D24 = statistics_file["timeseries/d24/$iter"][1, 1, :]
+        TE = statistics_file["timeseries/T/$iter"][1, 1, :]
+        C1  = statistics_file["timeseries/OH⁻/$iter"][1, 1, :]
+        # C3  = statistics_file["timeseries/c3/$iter"][1, 1, :]
+        # C12 = statistics_file["timeseries/c12/$iter"][1, 1, :]
+        # C24 = statistics_file["timeseries/c24/$iter"][1, 1, :]
+        # D1  = statistics_file["timeseries/d1/$iter"][1, 1, :]
+        # D3  = statistics_file["timeseries/d3/$iter"][1, 1, :]
+        # D12 = statistics_file["timeseries/d12/$iter"][1, 1, :]
+        # D24 = statistics_file["timeseries/d24/$iter"][1, 1, :]
 
-        wlim = 0.02
-        clim = 0.8
+#        wlim = 0.02
+        wlim = 0.8*maximum(abs, w)
 
-        cmax = maximum(abs, c1)
+
+        tmax = maximum(abs, TE)
+        tmin = minimum(abs, TE)
+        
+        clim = 0.8*maximum(abs, C1)
+        cmax = maximum(abs, C1)
 
         wlevels = nice_divergent_levels(w, wlim)
 
         clevels = cmax > clim ? vcat(range(0, stop=clim, length=40), [cmax]) :
                                      range(0, stop=clim, length=40)
 
-        T_plot = plot(T, zc, label="T", xlim=(initial_temperature(0, 0, -grid.Lz), θ_surface), legend=:bottom)
+        tlevels = range(tmin,tmax,length=20)
+        
+        T_plot = plot(TE, zc, label="T", xlim=(initial_temperature(0, 0, -grid.Lz), θ_surface), legend=:bottom)
 
         U_plot = plot([U, V, sqrt.(E)], zc, label=["u" "v" "√E"], linewidth=[1 1 2], legend=:bottom)
 
-        C_plot = plot([C1 C3 C12 C24 D1 D3 D12 D24], zc,
-                      label = ["C₁" "C₃" "C₁₂" "C₂₄" "D₁" "D₃" "D₁₂" "D₂₄"],
+        # C_plot = plot([C1 C3 C12 C24 D1 D3 D12 D24], zc,
+        #               label = ["C₁" "C₃" "C₁₂" "C₂₄" "D₁" "D₃" "D₁₂" "D₂₄"],
+        #               legend=:bottom,
+        #                xlim = (0, 1))
+        C_plot = plot([C1], zc,
+                      label = ["Hydroxide"],
                       legend=:bottom,
                        xlim = (0, 1))
-
         wxz_plot = contourf(xw, zw, w';
                                   color = :balance,
                             aspectratio = :equal,
                                   clims = (-wlim, wlim),
                                  levels = wlevels,
+                                  xlims = (0, grid.Lx),
+                                  ylims = (-grid.Lz, 0),
+                                 xlabel = "x (m)",
+                            ylabel = "z (m)")
+
+        Txz_plot = contourf(xT, zT, te';
+                                  color = :balance,
+                            aspectratio = :equal,
+                                  clims = (tmin, tmax),
+                                 levels = tlevels,
                                   xlims = (0, grid.Lx),
                                   ylims = (-grid.Lz, 0),
                                  xlabel = "x (m)",
@@ -360,30 +383,40 @@ if make_animation
                                  xlabel = "x (m)",
                                  ylabel = "z (m)")
 
-        d1xz_plot = contourf(xc, zc, d1';
-                                  color = :thermal,
-                            aspectratio = :equal,
-                                  clims = (0, clim),
-                                 levels = clevels,
-                                  xlims = (0, grid.Lx),
-                                  ylims = (-grid.Lz, 0),
-                                 xlabel = "x (m)",
-                                 ylabel = "z (m)")
+        # d1xz_plot = contourf(xc, zc, d1';
+        #                           color = :thermal,
+        #                     aspectratio = :equal,
+        #                           clims = (0, clim),
+        #                          levels = clevels,
+        #                           xlims = (0, grid.Lx),
+        #                           ylims = (-grid.Lz, 0),
+        #                          xlabel = "x (m)",
+        #                          ylabel = "z (m)")
 
         w_title = @sprintf("w(x, y=0, z, t=%s) (m/s)", prettytime(t))
         T_title = "T"
         c1_title = @sprintf("c₁(x, y=0, z, t=%s)", prettytime(t))
         U_title = "U and V"
-        d1_title = @sprintf("d₁(x, y=0, z, t=%s)", prettytime(t))
+        # d1_title = @sprintf("d₁(x, y=0, z, t=%s)", prettytime(t))
         C_title = "C's and D's"
 
-        plot(wxz_plot, T_plot, c1xz_plot, U_plot, d1xz_plot, C_plot, layout=(3, 2),
+        # plot(wxz_plot, T_plot, c1xz_plot, U_plot, d1xz_plot, C_plot, layout=(3, 2),
+        #      size = (1000, 1000),
+        #      link = :y,
+        #      title = [w_title T_title c1_title U_title d1_title C_title])
+
+        
+        plot(wxz_plot, T_plot, c1xz_plot, U_plot, C_plot, layout=(5, 1),
              size = (1000, 1000),
              link = :y,
-             title = [w_title T_title c1_title U_title d1_title C_title])
+             title = [w_title T_title c1_title U_title C_title])
 
+        fn = @sprintf("mov%s", iter)
+        png(fn)
+        
         iter == iterations[end] && close(file)
     end
 
-    gif(anim, prefix * ".gif", fps = 8)
+    
+#    gif(anim, prefix * ".gif", fps = 8)
 end
